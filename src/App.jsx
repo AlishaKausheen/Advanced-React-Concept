56f6f5e6868728ba2ffecc3efeea1933917c6f74
AlishaKausheen <alishakausheen@gmail.com>
2023-11-08 04:35:46 -0800

    learnt useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React, {useState} from "react";
 
 function App() {
   const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('blue');
   function decrementCount() {
     setCount(prevCount => prevCount - 1);
-}
-  return (
+    setTheme('red');
+  }
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+    setTheme('green');
+  }
+return(
     <div className="container">
     
       <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button>+</button>
+    <span>{count}</span>
+    <span>{theme}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
     )
 }
