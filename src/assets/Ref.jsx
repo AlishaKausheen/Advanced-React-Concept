c41985fb8750a3b87dd9148fca1f8d1941281e8b
AlishaKausheen <alishakausheen@gmail.com>
2023-11-12 06:31:59 -0800

    useRef

--- src/assets/Ref.jsx
+++ src/assets/Ref.jsx
@@ -2,14 +2,20 @@ import React, { useState,useEffect , useRef } from "react";
 export const Ref = () => {
     const [name, setName] = useState('');
     const renderCount = useRef(1);
+    const inputRef= useRef()
     useEffect(() => {
         renderCount.current = renderCount.current + 1;
     })
+    function focus() {
+        inputRef.current.focus();
+        
+    }
     return (
         <div>
-            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+            <input ref={inputRef} type="text" value={name} onChange={e => setName(e.target.value)} />
             <div>My name is {name}</div>
             <div>I rendered {renderCount.current}</div>
+            <button onClick={focus}>Focus</button>
         </div>
     )
 }
