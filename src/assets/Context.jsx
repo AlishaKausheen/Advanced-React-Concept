cb32b006226a937694d94c6de0f68c0ce3be9d00
AlishaKausheen <alishakausheen@gmail.com>
2023-11-13 22:50:07 -0800

    Context 3

--- src/assets/Context.jsx
+++ src/assets/Context.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { ClassContextComponent } from "./ClassContextComponent";
 import { FunctionContextComponent } from "./FunctionContextComponent";
+import { ThemeProvider } from "./ThemeContext";
 export const ThemeContext= React.createContext()
 export const Context = () => {
     const [darkTheme, setDarkTheme] = useState(true);
@@ -9,11 +10,17 @@ export const Context = () => {
     }
     return (
         <>
+
             <ThemeContext.Provider value={darkTheme}>
                 <button onClick={toggleTheme}>Toggle Theme</button>
                 <FunctionContextComponent />
                 <ClassContextComponent />
             </ThemeContext.Provider>
+            <ThemeProvider>
+                <button onClick={toggleTheme}>Toggle Theme</button>
+                <FunctionContextComponent />
+                <ClassContextComponent />
+            </ThemeProvider>
         </>
     )
 }
